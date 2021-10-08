f7c3324e25d6c4d8e90670d1a020078e2056a417
etuncay <etuncay@subu.edu.tr>
2021-10-08 08:33:35 +0300

    accordion component oluşturuldu

---
+++ src/components/accordion/TblrAccordion.svelte
@@ -0,0 +1,7 @@
+<div class="card">
+	<div class="card-body">
+		<div class="accordion" id="accordion-example">
+			<slot></slot>
+		</div>
+	</div>
+</div>
---
+++ src/components/accordion/TblrAccordionItem.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { v4 as uuidv4 } from 'uuid';
+	export  let title=''
+	export let expanded=false
+
+	let id = uuidv4()
+</script>
+
+<div class="accordion-item">
+	<h2 class="accordion-header" id="heading-{id}">
+		<button class="accordion-button {expanded?'':'collapsed'}" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-{id}" aria-expanded="true">
+			{title}
+		</button>
+	</h2>
+	<div id="collapse-{id}" class="accordion-collapse collapse {expanded?'show':''}" data-bs-parent="#accordion-example">
+		<div class="accordion-body pt-0">
+			<slot></slot>
+		</div>
+	</div>
+</div>
